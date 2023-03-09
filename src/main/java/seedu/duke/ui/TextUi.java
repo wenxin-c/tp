982b72eee12f2df206269af862dc3e740a803a83
wenxin-c <chenwenxin0825@gmail.com>
2023-03-09 21:32:26 +0800

    Add UI class for input and output

---
+++ src/main/java/seedu/duke/ui/TextUi.java
@@ -0,0 +1,72 @@
+package seedu.duke.ui;
+
+import java.util.Scanner;
+
+/**
+ * UI class for reading user inputs and printing outputs
+ */
+public class TextUi {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String SEPARATOR = "-";
+    private static final String INDENTATION_SPACES = "    ";
+
+    /**
+     * Read user's input command and return back the command string.<br/>
+     *
+     * @return user input command with leading/dangling whitespace being removed
+     */
+    public static String getCommand() {
+        String inputLine;
+        String userCommand;
+        inputLine = SCANNER.nextLine();
+        userCommand = inputLine.trim();
+        return userCommand;
+    }
+
+    /**
+     * Print line separators for output lines.<br/>
+     * Each subclass inherited from this class can override this method to vary the interface.<br/>
+     *
+     * @param numOfChars determine the length of separation line to accommodate for different length of output
+     */
+    public static void printSeparator(int numOfChars) {
+        for (int i=0; i<numOfChars; ++i) {
+            System.out.print(SEPARATOR);
+        }
+        System.out.print(System.lineSeparator());
+    };
+
+    /**
+     * Print spaces before output message for better formatting.
+     */
+    public static void printIndentation() {
+        System.out.print(INDENTATION_SPACES);
+    }
+
+    /**
+     * Print exception message.
+     * Can override to accommodate to other customised error messages.
+     *
+     * @param exception the exception being thrown in the program
+     */
+    public static void printErrorFor(Exception exception) {
+        printSeparator(40);
+        printIndentation();
+        System.out.println(exception.getMessage());
+        printSeparator(40);
+    }
+
+    /**
+     * Print exception message.
+     * Can override to accommodate to other customised error messages.
+     *
+     * @param message the exception being thrown in the program
+     */
+    public static void printOutputMessage(String message) {
+        printSeparator(40);
+        printIndentation();
+        System.out.println(message);
+        printSeparator(40);
+    }
+}
+
---
+++ src/test/java/seedu/duke/ui/TextUiTest.java
@@ -0,0 +1,69 @@
+package seedu.duke.ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.duke.ui.TextUi.getCommand;
+import static seedu.duke.ui.TextUi.printErrorFor;
+import static seedu.duke.ui.TextUi.printOutputMessage;
+
+
+class TextUiTest {
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void getCommand_trimSpace_success() {
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream(" My string ".getBytes());
+        System.setIn(in);
+        String command = getCommand();
+        assertEquals("My string", command);
+        System.setIn(sysInBackup);
+    }
+
+    @Test
+    void printSeparator_defaultVersion_success() {
+        int numOfChar = 5;
+        TextUi.printSeparator(numOfChar);
+        assertEquals("-----", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void printErrorFor_ArithmeticException_success() {
+        try {
+            int result = 2/0;
+        } catch (ArithmeticException exception) {
+            printErrorFor(exception);
+        }
+        assertEquals("--------------------"+
+                    "--------------------"+System.lineSeparator()+
+                    "    / by zero"+System.lineSeparator()+
+                    "--------------------"+
+                    "--------------------",
+                    outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void printOutputMessage_greeting_success() {
+        String greeting = "How are you?";
+        printOutputMessage(greeting);
+        assertEquals("--------------------" +
+                "--------------------" + System.lineSeparator()+
+                "    How are you?" + System.lineSeparator() +
+                "--------------------" +
+                "--------------------", outputStreamCaptor.toString().trim());
+    }
+
+}
+
