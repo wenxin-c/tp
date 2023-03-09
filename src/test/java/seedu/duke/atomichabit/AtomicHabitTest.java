9d14122c94fa4dfb836a0dd557a18365d52a0850
YongbinWang <yongbin_01@hotmail.com>
2023-03-10 00:44:55 +0800

    Added test package and file format

---
+++ src/test/java/seedu/duke/atomichabit/AtomicHabitTest.java
@@ -0,0 +1,17 @@
+package seedu.duke.atomichabit;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.atomichabit.atomichabit.AtomicHabitManager;
+
+
+public class AtomicHabitTest {
+
+    /**
+     * Will fill with tests after discussion
+     */
+    @Test
+    public void addhabit_correct_output() {
+
+    }
+
+}
