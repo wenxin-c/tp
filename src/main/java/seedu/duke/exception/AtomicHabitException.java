97c61128e50193e28bc62e5c289b9f2528897edf
YongbinWang <yongbin_01@hotmail.com>
2023-03-11 11:25:01 +0800

    Remove unused package

--- src/main/java/seedu/duke/exception/AtomicHabitException.java
+++ src/main/java/seedu/duke/exception/AtomicHabitException.java
@@ -1,7 +1,5 @@
 package seedu.duke.exception;
 
-import seedu.duke.exception.WellNusException;
-
 public class AtomicHabitException extends WellNusException {
     public AtomicHabitException(String errorMessage) {
         super(errorMessage);
