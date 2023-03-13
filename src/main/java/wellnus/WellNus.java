3b0f38fa298325a7c3f69d915a94780f43329c7f
Wang Haoyang <haoyangw@gmail.com>
2023-03-13 22:17:00 +0800

    Replace hardcoded WellNus logo
    Abstract out the WellNus logo into a private static method
      so its more maintainable but just as elegant.

--- src/main/java/wellnus/WellNus.java
+++ src/main/java/wellnus/WellNus.java
@@ -3,22 +3,24 @@ package wellnus;
 import java.util.Scanner;
 
 public class WellNus {
+    private static final String NEWLINE = System.lineSeparator();
+
+    private static String getWellnusLogo() {
+        return NEWLINE
+                + ",--.   ,--.       ,--.,--.,--.  ,--.,--. ,--. ,---.     |  |        |  |     " + NEWLINE
+                + "|  |   |  | ,---. |  ||  ||  ,'.|  ||  | |  |'   .-',---|  |---.,---|  |---. " + NEWLINE
+                + "|  |.'.|  || .-. :|  ||  ||  |' '  ||  | |  |`.  `-.'---|  |---''---|  |---' " + NEWLINE
+                + "|   ,'.   |\\   --.|  ||  ||  | `   |'  '-'  '.-'    |   |  |        |  |     " + NEWLINE
+                + "'--'   '--' `----'`--'`--'`--'  `--' `-----' `-----'    `--'        `--'     " + NEWLINE;
+    }
+
     /**
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = "\n" +
-                ",--.   ,--.       ,--.,--.,--.  ,--.,--. ,--. ,---.     |  |        |  |     \n" +
-                "|  |   |  | ,---. |  ||  ||  ,'.|  ||  | |  |'   .-',---|  |---.,---|  |---. \n" +
-                "|  |.'.|  || .-. :|  ||  ||  |' '  ||  | |  |`.  `-.'---|  |---''---|  |---' \n" +
-                "|   ,'.   |\\   --.|  ||  ||  | `   |'  '-'  '.-'    |   |  |        |  |     \n" +
-                "'--'   '--' `----'`--'`--'`--'  `--' `-----' `-----'    `--'        `--'     \n" ;
-
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("Hello " + in.nextLine());
+        System.out.println("Hello from" + System.lineSeparator());
+        System.out.println(WellNus.getWellnusLogo());
     }
+
 }
 
