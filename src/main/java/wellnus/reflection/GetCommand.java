5f53b199540e63ea21d351cd391901762be2bb1a
wenxin-c <chenwenxin0825@gmail.com>
2023-03-17 00:11:18 +0800

    Add dot after index for reflection questions

--- src/main/java/wellnus/reflection/GetCommand.java
+++ src/main/java/wellnus/reflection/GetCommand.java
@@ -25,6 +25,8 @@ public class GetCommand extends Command {
     private static final String COMMAND_KEYWORD_ASSERTION = "The key should be get.";
     private static final String COMMAND_PAYLOAD_ASSERTION = "The payload should be empty.";
     private static final String NUM_SELECTED_QUESTIONS_ASSERTION = "The number of selected questions should be 5.";
+    private static final String DOT = ".";
+    private static final int ONE_OFFSET = 1;
     private static final ReflectUi UI = new ReflectUi();
     private HashMap<String, String> argumentPayload;
 
@@ -146,7 +148,7 @@ public class GetCommand extends Command {
      * @return Array of 5 random numbers
      */
     private Set<Integer> generateRandomNumbers(int maxSize) {
-        Set<Integer> randomNumbers = new Random().ints(MIN_SIZE, maxSize - 1)
+        Set<Integer> randomNumbers = new Random().ints(MIN_SIZE, maxSize - ONE_OFFSET)
                 .distinct()
                 .limit(NUM_OF_RANDOM_QUESTIONS)
                 .boxed()
@@ -164,7 +166,7 @@ public class GetCommand extends Command {
         ArrayList<ReflectionQuestion> selectedQuestions = getRandomQuestions();
         String questionString = "";
         for (int i = 0; i < selectedQuestions.size(); i += 1) {
-            questionString += (Integer.toString(i + 1) + selectedQuestions.get(i).toString()
+            questionString += (Integer.toString(i + ONE_OFFSET) + DOT + selectedQuestions.get(i).toString()
                     + System.lineSeparator());
         }
         return questionString;
