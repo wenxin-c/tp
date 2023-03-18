c4ecc8282117983eca9e3fe6e3359ab0e3dcecc0
nichyjt <nicholas.yek@gmail.com>
2023-03-18 11:41:47 +0800

    Add Javadoc for AtomicHabitException

--- src/main/java/wellnus/exception/AtomicHabitException.java
+++ src/main/java/wellnus/exception/AtomicHabitException.java
@@ -1,5 +1,12 @@
 package wellnus.exception;
 
+/**
+ * AtomicHabitException is thrown when a conceptual/logical error occurs in the AtomicHabit package <br>
+ * <p>
+ * This exception should only be used within the AtomicHabit package.
+ * It differentiates between WellNUS and regular Java exceptions,
+ * which allows better pinpointing of errors to the AtomicHabit logic.
+ */
 public class AtomicHabitException extends WellNusException {
     public AtomicHabitException(String errorMessage) {
         super(errorMessage);
