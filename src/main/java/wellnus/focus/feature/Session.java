9b58c1ba83ecf10d2d2c1cc8949ffeb6788f0f36
nichyjt <nicholas.yek@gmail.com>
2023-03-27 11:04:58 +0800

    Add state checks and fix buggy state logic

--- src/main/java/wellnus/focus/feature/Session.java
+++ src/main/java/wellnus/focus/feature/Session.java
@@ -6,18 +6,32 @@ import java.util.ArrayList;
 /**
  * Represents a session of Countdown objects.
  * A session is a sequence of Countdown objects represented by an ArrayList.
+ * We define a Session to have 4 states.
+ * <ol>
+ *     <li> Ready
+ *          `config` and `start` are only legal here.*
+ *     <li> Counting
+ *          `pause` and `stop` are only legal here
+ *     <li> Waiting
+ *          `next` and `stop` are only legal here
+ *     <li> Paused
+ *          `resume` and `stop` are only legal here
+ * </ol>
+ * The first timer is a dummy timer
  */
+//@@author YongbinWang
 public class Session {
     private static final int INCREMENT = 1;
-    private final ArrayList<Countdown> session;
+    private static final boolean IS_LAST_COUNTDOWN = true;
     private String workDescription = "Task";
+    private final ArrayList<Countdown> session;
     private String breakDescription = "Break";
     private String longBreakDescription = "Long Break";
     private int work = 1;
     private int brk = 1;
     private int longBrk = 1;
     private int cycle = 2;
-    private int currentCountdownIndex = 0;
+    private int currentCountdownIndex;
 
     /**
      * Constructs a Session object.
@@ -26,6 +40,8 @@ public class Session {
      */
     public Session() {
         this.session = new ArrayList<>();
+        initialiseSession();
+        this.currentCountdownIndex = session.size() - INCREMENT;
     }
 
     /**
@@ -33,12 +49,12 @@ public class Session {
      */
     private void fillSession() {
         for (int i = 0; i < cycle; i++) {
-            Countdown workCountDown = new Countdown(work, workDescription);
-            Countdown breakCountDown = new Countdown(brk, breakDescription);
+            Countdown workCountDown = new Countdown(work, workDescription, !IS_LAST_COUNTDOWN);
+            Countdown breakCountDown = new Countdown(brk, breakDescription, !IS_LAST_COUNTDOWN);
             session.add(workCountDown);
             session.add(breakCountDown);
         }
-        Countdown longBreak = new Countdown(longBrk, longBreakDescription);
+        Countdown longBreak = new Countdown(longBrk, longBreakDescription, IS_LAST_COUNTDOWN);
         int lastIndex = session.size() - 1;
         session.remove(lastIndex);
         session.add(longBreak);
@@ -66,25 +82,56 @@ public class Session {
      * Method to increment the current countdown index if the current countdown is completed.
      */
     public void checkPrevCountdown() {
-        if (session.size() == 0) {
+        if (getCurrentCountdown().getIsReady()) {
+            initialiseSession();
+            currentCountdownIndex = 0;
             return;
         }
-        if (session.get(currentCountdownIndex).getIsCompletedCountdown()
-                && currentCountdownIndex < session.size() - INCREMENT) {
-            currentCountdownIndex += INCREMENT;
-        }
-        if (session.get(currentCountdownIndex).getIsCompletedCountdown()
-                && currentCountdownIndex == session.size() - INCREMENT) {
-            currentCountdownIndex = 0;
-        }
+        currentCountdownIndex += INCREMENT;
+
+    }
+
+    public Countdown getCurrentCountdown() {
+        return session.get(currentCountdownIndex);
+    }
+
+    public boolean isSessionReady() {
+        return getCurrentCountdownIndex() == session.size() - INCREMENT && getCurrentCountdown().getIsReady();
+    }
+
+    public boolean isSessionCounting() {
+        Countdown countdown = getCurrentCountdown();
+        return countdown.getIsRunning() && !countdown.getIsCompletedCountdown();
+    }
+
+    public boolean isSessionWaiting() {
+        Countdown countdown = getCurrentCountdown();
+        return !countdown.getIsRunning() && countdown.getIsCompletedCountdown();
+    }
+
+    public boolean isSessionPaused() {
+        Countdown countdown = getCurrentCountdown();
+        return !countdown.getIsRunning() && !countdown.getIsCompletedCountdown() && !countdown.getIsReady();
+    }
+
+    public void startTimer() {
+        checkPrevCountdown();
+        getCurrentCountdown().setStart();
+        getCurrentCountdown().start();
     }
 
     /**
-     * Method to initialise a session when start command is executed.
+     * Method to (re) initialise a session when start or stop command is executed.
      */
-    public void init() {
+    public void initialiseSession() {
         session.clear();
         fillSession();
+        primeSessionIsReady();
+    }
+
+    private void primeSessionIsReady() {
+        int lastIndex = session.size() - INCREMENT;
+        session.get(lastIndex).setIsReady(true);
     }
 
     public boolean hasAnyCountdown() {
@@ -96,7 +143,8 @@ public class Session {
      * This method is called when the user wants to stop an ongoing session.
      */
     public void resetCurrentCountdownIndex() {
-        currentCountdownIndex = 0;
+        currentCountdownIndex = session.size() - INCREMENT;
+        primeSessionIsReady();
     }
 
     public int getWork() {
@@ -113,6 +161,8 @@ public class Session {
 
     public void setCycle(int newCycles) {
         this.cycle = newCycles;
+        fillSession();
+        currentCountdownIndex = session.size() - INCREMENT;
     }
 
     public int getBrk() {
@@ -130,4 +180,5 @@ public class Session {
     public void setLongBrk(int newLongBrk) {
         this.longBrk = newLongBrk;
     }
+
 }
