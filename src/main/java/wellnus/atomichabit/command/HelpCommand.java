ba5887fa74b149a1fb8359769996b65567d89b00
BernardLesley <bernardlesleyefendy@gmail.com>
2023-04-06 07:00:49 +0800

    Add missing javadoc for help command

--- src/main/java/wellnus/atomichabit/command/HelpCommand.java
+++ src/main/java/wellnus/atomichabit/command/HelpCommand.java
@@ -88,8 +88,9 @@ public class HelpCommand extends Command {
     }
 
     /**
-     * Prints the help message for a given commandToSearch.
-     * If it does not exist,
+     * Prints the help message for a given commandToSearch. <br/>
+     * If the commandToSearch does not exist, helpCommand will print an unknown command
+     * error message.
      */
     public void printSpecificHelpMessage(String commandToSearch) {
         switch (commandToSearch) {
